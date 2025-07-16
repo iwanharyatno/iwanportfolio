8259e7b9b17c236b50e191b252e6f640da3919e3
Iwan Haryatno <iwnharry61@gmail.com>
2025-07-16 10:22:04 +0700

    add: azure depth background

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,23 @@ import Footer from './components/Footer';
 
 function App() {
     return (
-        <div className="bg-[#0B101C] text-white pt-6 px-4">
-            <Navbar />
-            <Hero />
-            <Services />
-            <Technologies />
-            <Portfolio />
-            <Contact />
-            <Footer />
+        <div className="min-h-screen w-full relative">
+            {/* Azure Depths */}
+            <div
+                className="absolute inset-0 -z-10"
+                style={{
+                    background: "radial-gradient(125% 125% at 50% 100%, #000000 40%, #010133 100%)",
+                }}
+            />
+            <div className='text-white pt-6 px-4'>
+                <Navbar />
+                <Hero />
+                <Services />
+                <Technologies />
+                <Portfolio />
+                <Contact />
+                <Footer />
+            </div>
         </div>
     );
 }
