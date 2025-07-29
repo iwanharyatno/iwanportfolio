be2bed7aa9fb6ea31939de2154b21d12c564c958
Iwan Haryatno <iwnharry61@gmail.com>
2025-07-29 09:27:36 +0700

    update: footer year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className="max-w-7xl mx-auto px-4 py-6 text-[10px] text-[#8B8B8B] tracking-widest text-center">
-            © 2023. All rights reserved, web design and development - Iwan Haryatno
+            © 2025. All rights reserved, web design and development - Iwan Haryatno
         </footer>
     );
 }
